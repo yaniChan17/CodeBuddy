62100b68ec034d7d6ec094ea4f71c1a7da23dc98
johnpaul1207 <johnpaulrguez1290@gmail.com>
2025-10-14 16:11:08 +0800

    Text rana Kol

--- frontend/App.js
+++ frontend/App.js
@@ -1,3 +1,5 @@
+Hello Buang
+
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
